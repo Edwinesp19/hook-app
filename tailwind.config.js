2faef523189c27a2f75bd1a8ee0685ab554bad87
Edwin Espinal <edwinespinal1999@gmail.com>
2023-11-19 16:02:05 -0400

    todo design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Comfortaa: ["Comfortaa", "sans-serif"],
+       },
+    },
   },
   plugins: [
     require('tailwindcss-animated')
