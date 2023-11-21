d784e421c86c0ec0ee9a938939769c0e80a35f2e
Edwin Espinal <edwinespinal1999@gmail.com>
2023-11-21 09:34:15 -0400

    customize wallpaper background

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,64 +1,20 @@
-import { useReducer, useEffect, useState} from "react"
-import { todoReducer } from "./todoReducer";
 import { TodoList } from "./TodoList";
 import { TodoClock } from "./TodoClock";
 import { WelcomeText } from "./WelcomeText";
 import { TodoAdd } from "./TodoAdd";
+import { useTodos } from "../hooks/index";
 
-
-
-
-const initialState = [
-    // {
-    //     id:new Date().getTime(),
-    //     description:'Recolectar la piedra del alma',
-    //     done:false,
-    // },
-];
- 
-const init=()=>{
-    return JSON.parse(localStorage.getItem('todos')) ||[];
-}
 
 export default function TodoApp() {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState,init);
-
-    useEffect(() => {
-      localStorage.setItem('todos',JSON.stringify(todos));
-    
-    }, [todos])
-    
-
-    const handleNewTodo =(todo)=>{
-        const action = {
-            type:'[TODO] Add Todo',
-            payload:todo,
-        }
-
-         dispatch(action);
-    }
-
-    const handleDeleteTodo=(id)=>{
-        dispatch({
-            type:'[TODO] Remove Todo',
-            payload:id,
-        });
-    }
-
-    const handleCheckedTodo=(id)=>{
-        dispatch({
-            type:'[TODO] Checked Todo',
-            payload:id,
-        });
-    }
+    const {todos, backgroundImage, handleNewTodo,handleDeleteTodo,handleCheckedTodo} = useTodos();
 
 
-  return (
+    return (
     <>
-     <section className="bg-[url(https://images.unsplash.com/photo-1500829243541-74b677fecc30?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-center w-screen flex items-center justify-center gap-3 h-screen ">
-
-
+   
+     <section className="relative bg-center w-screen flex items-center justify-center gap-3 h-screen">
+       <img className=" absolute w-full h-full" src={backgroundImage} alt="wallpaper" />
      <div className="grid grid-cols-[1fr,1fr] h-full p-4 pb-6 w-full gap-4 max-w-[1300px]">
        <TodoClock/>
             <div className="animate-fade-left animate-delay-100 animate-once animate-ease-in-out animate-alternate h-[95vh] w-full grid grid-rows-[120px,auto,50px] gap-2 p-6 border-[1px] border-white/20 bg-black/25 backdrop-blur-lg rounded-3xl">
@@ -84,6 +40,7 @@ export default function TodoApp() {
         </div>
       
     </section>
+
     </>
   )
 }
---
+++ src/hooks/useTodos.js
@@ -0,0 +1,68 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+ 
+const init=()=>{
+    return JSON.parse(localStorage.getItem('todos')) ||[]; // carga el listado desde el localstorage, si no hay pues lo deja vacio 
+}
+
+const images = [
+    'https://images.unsplash.com/photo-1420745981456-b95fe23f5753?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MzEwNDl8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDA1NzA0NDZ8&ixlib=rb-4.0.3&q=85',
+    'https://images.unsplash.com/photo-1500829243541-74b677fecc30?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1950&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1440964829947-ca3277bd37f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1552083375-1447ce886485?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+];
+
+const randomIndex = Math.floor(Math.random() * images.length);
+
+const backgroundImage = images[randomIndex];
+
+export const useTodos = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState,init);
+
+    
+
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todos)); //guarda el listado en el local storage
+    
+    }, [todos])
+    
+
+    const handleNewTodo =(todo)=>{
+        const action = {
+            type:'[TODO] Add Todo',
+            payload:todo,
+        }
+
+         dispatch(action);
+    }
+
+    const handleDeleteTodo=(id)=>{
+        dispatch({
+            type:'[TODO] Remove Todo',
+            payload:id,
+        });
+    }
+
+    const handleCheckedTodo=(id)=>{
+        dispatch({
+            type:'[TODO] Checked Todo',
+            payload:id,
+        });
+    }
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleCheckedTodo,
+    backgroundImage
+  }
+   
+  
+}
